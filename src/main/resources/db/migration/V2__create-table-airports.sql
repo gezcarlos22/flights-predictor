a9266411849287653edc67958b2747f3626fc015
Michael Ziliani <zilianimichael@gmail.com>
2026-01-05 15:43:12 -0500

    Update airport table

--- src/main/resources/db/migration/V2__create-table-airports.sql
+++ src/main/resources/db/migration/V2__create-table-airports.sql
@@ -1,14 +1,15 @@
 create table airports (
 
     id bigint not null  auto_increment,
-    country_name varchar(20),
-    city_name varchar(50) not null,
+    airport_iata char(3),
     airport_name varchar(150) not null,
-    airport_iata char(3) unique,
-    city_iata char(3) unique,
+    country_name varchar(50) not null,
+    city_name varchar(100)not null,
     latitude decimal(9.6) not null,
     longitude decimal(9.6) not null,
-    timezone varchar(50) not null,
+    elevation int(4),
+    timezone varchar(50),
+    googleMaps varchar(255),
 
     primary key(id)
 
